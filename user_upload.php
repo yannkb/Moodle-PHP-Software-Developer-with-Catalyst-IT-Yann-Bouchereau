46ff5285824104649743048738078788005ce6f0
Yann Bouchereau <yann.bchr@gmail.com>
2023-12-09 14:51:36 +1100

    📦 NEW: add create table option

--- user_upload.php
+++ user_upload.php
@@ -2,6 +2,7 @@
 
 // Extract options
 $csvFilePath = $options['file'];
+$createTable = isset($options['create_table']);
 $dryRun = isset($options['dry_run']);
 $dbUser = $options['u'];
 $dbPass = $options['p'];
@@ -9,6 +10,31 @@ $dbHost = $options['h'];
 
 // Database configuration
 $dbName = 'app';
+
+// Create or rebuild the users table if necessary
+if ($createTable) {
+    $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "DROP TABLE IF EXISTS users;
+            CREATE TABLE users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(255),
+                surname VARCHAR(255),
+                email VARCHAR(255)
+            )";
+
+    if ($conn->multi_query($sql)) {
+        echo "Table 'users' created/rebuilt successfully.\n";
+    } else {
+        echo "Error creating/rebuilding table: " . $conn->error . "\n";
+        exit(1);
+    }
+
+    $conn->close();
+}
 
 // Validate and insert data from CSV file into the database (if not in dry run mode)
 if (!$dryRun && ($handle = fopen($csvFilePath, "r")) !== false) {
